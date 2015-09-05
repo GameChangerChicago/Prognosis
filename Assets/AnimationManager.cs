c32225e043eb5f3064d4ca82016609ac40263208
kaira2006 <kayura60@gmail.com>
2015-09-05 01:29:58 -0500

    Credit Screen Functionally complete

--- Assets/AnimationManager.cs
+++ Assets/AnimationManager.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class AnimationManager : MonoBehaviour {
 
 	public GameObject CreditsSky, CreditCityExtraLarge, CreditTreeMedium_1, 
-					  CreditCityMedium_2, MotionBlur, MainMenuScreen, PrognosisTitle, CreditText, CreditScreen;
+					  CreditCityMedium_2, MotionBlur, MainMenuScreen, PrognosisTitle,
+					  CreditText, CreditScreen, NewGameButton, CreditButton, HowToPlayButton, 
+					  CreditsBackButton;
+
 
 
 
@@ -14,26 +17,43 @@ public class AnimationManager : MonoBehaviour {
 
 	private IEnumerator rollCredits (){
 
-
-		MotionBlur.SetActive(true);
 		MotionBlur.GetComponent<Animator>().Play ("FadeIn");
 		yield return new WaitForSeconds(1);
-		MainMenuScreen.SetActive (false);
-		CreditScreen.SetActive (true);
+		MenuScreenActive (false);
+		CreditScreenActive (true);
 		MotionBlur.GetComponent<Animator> ().Play ("FadeOut");
-		yield return new WaitForSeconds(1);
-		//MotionBlur.SetActive (false);
-		CreditText.SetActive (true);
-		CreditText.GetComponent<Animator> ().Play ("RollCredits");
-		yield return new WaitForSeconds(25);
-		MotionBlur.GetComponent<Animator> ().Play ("MotionBlurReverse");
-		CreditsSky.GetComponent<Animator> ().Play ("MoveLeft");
+		CreditText.GetComponent<Animator> ().Play ("RollCredits", -1, 0.0f);
+		yield return new WaitForSeconds(32);
+		MotionBlur.GetComponent<Animator> ().Play ("FadeIn");
+		yield return new WaitForSeconds (1);
+		MenuScreenActive (true);
+		CreditScreenActive (false);
+		MotionBlur.GetComponent<Animator> ().Play ("FadeOut");
+
 	}
 
 	
 	public void CoroutineWrapper(string name){
 		StartCoroutine (name);
 	}
+
+
+	public void MenuScreenActive(bool b){
+
+		MainMenuScreen.SetActive (b);
+		CreditButton.SetActive (b);
+		CreditsBackButton.SetActive (!b);
+		HowToPlayButton.SetActive (b);
+		NewGameButton.SetActive (b);
+
+	}
+
+	public void CreditScreenActive(bool b){
+		CreditScreen.SetActive (b);
+		CreditText.SetActive (b);
+
+	}
+
 
 
 
