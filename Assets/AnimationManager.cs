8b1b3c85af662bc930db61dc054802b00eb91ea3
kaira2006 <kayura60@gmail.com>
2015-09-04 22:52:35 -0500

    Setting up final animations

---
+++ Assets/AnimationManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+	public GameObject CreditsSky, CreditCityExtraLarge, CreditTreeMedium_1, 
+					  CreditCityMedium_2, MotionBlur, MainMenuScreen, PrognosisTitle, CreditText, CreditScreen;
+
+
+
+	// Use this for initialization
+
+
+
+	private IEnumerator rollCredits (){
+
+
+		MotionBlur.SetActive(true);
+		MotionBlur.GetComponent<Animator>().Play ("FadeIn");
+		yield return new WaitForSeconds(1);
+		MainMenuScreen.SetActive (false);
+		CreditScreen.SetActive (true);
+		MotionBlur.GetComponent<Animator> ().Play ("FadeOut");
+		yield return new WaitForSeconds(1);
+		//MotionBlur.SetActive (false);
+		CreditText.SetActive (true);
+		CreditText.GetComponent<Animator> ().Play ("RollCredits");
+		yield return new WaitForSeconds(25);
+		MotionBlur.GetComponent<Animator> ().Play ("MotionBlurReverse");
+		CreditsSky.GetComponent<Animator> ().Play ("MoveLeft");
+	}
+
+	
+	public void CoroutineWrapper(string name){
+		StartCoroutine (name);
+	}
+
+
+
+}
