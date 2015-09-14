099c75735668f6f7437df3aae3d536515902956b
mwarrington <mason.arrington@gmail.com>
2015-09-14 15:32:41 -0500

    Winning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,81 @@ public class GameManager : MonoBehaviour
                  Education;
     public bool DragObjectInstantiated;
 
+    #region World Health Rates
+    public float WorldSTIRate
+    {
+        get
+        {
+            float totalSTIRate = 0;
+            int targetLocationCount = 0;
+
+            foreach (TargetLocation tl in FindObjectsOfType<TargetLocation>())
+            {
+                totalSTIRate += tl.STIRate;
+                targetLocationCount++;
+            }
+
+            _worldSTIRate = totalSTIRate / targetLocationCount;
+
+            return _worldSTIRate;
+        }
+
+        set
+        {
+            Debug.Log("This property shouldn't be being set");
+        }
+    }
+    private float _worldSTIRate;
+    public float WorldTeenPregRate
+    {
+        get
+        {
+            float totalTeenPregRate = 0;
+            int targetLocationCount = 0;
+
+            foreach (TargetLocation tl in FindObjectsOfType<TargetLocation>())
+            {
+                totalTeenPregRate += tl.TeenPregRate;
+                targetLocationCount++;
+            }
+
+            _worldTeenPregRate = totalTeenPregRate / targetLocationCount;
+
+            return _worldTeenPregRate;
+        }
+
+        set
+        {
+            Debug.Log("This property shouldn't be being set");
+        }
+    }
+    private float _worldTeenPregRate;
+    public float WorldCommHealthRate
+    {
+        get
+        {
+            float totalCommHealthRate = 0;
+            int targetLocationCount = 0;
+
+            foreach (TargetLocation tl in FindObjectsOfType<TargetLocation>())
+            {
+                totalCommHealthRate += tl.CommunityHealth;
+                targetLocationCount++;
+            }
+
+            _worldCommHealthRate = totalCommHealthRate / targetLocationCount;
+
+            return _worldCommHealthRate;
+        }
+
+        set
+        {
+            Debug.Log("This property shouldn't be being set");
+        }
+    }
+    private float _worldCommHealthRate;
+    #endregion
+
     public int CurrentTurn
     {
         get
@@ -23,9 +98,30 @@ public class GameManager : MonoBehaviour
                 {
                     tl.UpdateValues();
                 }
+
+                if (WorldTeenPregRate < 21)
+                {
+                    _goalTurnCount++;
+
+                    if (_goalTurnCount > 15)
+                    {
+                        Debug.Log("YOU WIN!!!!!");
+                    }
+                }
+                else
+                {
+                    _goalTurnCount = 0;
+                }
             }
             _currentTurn = value;
         }
     }
     private int _currentTurn;
+
+    private int _goalTurnCount;
+
+    void Update()
+    {
+        
+    }
 }
