19d805f26fc1360e5c164d85269d9f6b0716d06d
mwarrington <mason.arrington@gmail.com>
2017-11-06 16:07:38 -0600

    Better control

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private List<string> _recentClicks;
+
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            ClickHandler();
+            _recentClicks.Clear();
+        }
+    }
+
+    public void ClickTry(string clickableName)
+    {
+        _recentClicks.Add(clickableName);
+    }
+
+    private void ClickHandler()
+    {
+        if(_recentClicks.Contains("Pause") || _recentClicks.Contains("Skip"))
+        {
+
+        }
+        else if(_recentClicks.Contains("ProfWindow") || _recentClicks.Contains("Up") || _recentClicks.Contains("Down"))
+        {
+
+        }
+    }
+}
