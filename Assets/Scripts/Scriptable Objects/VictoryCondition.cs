21fe0c6566446a07ec80eff1191937a13f7e151b
mwarrington <mason.arrington@gmail.com>
2017-11-29 16:11:30 -0600

    Start of victory conditions

---
+++ Assets/Scripts/Scriptable Objects/VictoryCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryCondition : ScriptableObject
+{
+    public VictoryType myVictoryType;
+
+}
+
+public enum VictoryType
+{
+    MAINTAIN,
+    ACHIEVE
+}
+
+public enum Stats
+{
+    STIRATE,
+    TEENPREGRATE,
+    CRIMERATE
+}
