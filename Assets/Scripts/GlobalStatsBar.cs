136690ba957e403ce8ea01ccdd8ebeecb5216794
mwarrington <mason.arrington@gmail.com>
2017-12-11 15:14:01 -0600

    Colored bars

--- Assets/Scripts/GlobalStatsBar.cs
+++ Assets/Scripts/GlobalStatsBar.cs
@@ -4,55 +4,110 @@ using System.Collections;
 
 public class GlobalStatsBar : MonoBehaviour {
 
-	public Image worldTeenPregRate, worldSTIRate, worldCommHealthRate, financeBar, educationBar;
+	public Image worldTeenPregRate, worldSTIRate, worldCrimeRate, financeBar, educationBar;
 	private GameManager gameManager;
-	private float changeOverTime = 5.0f;
+	private float changeOverTime = 15.0f;
 	private TimerController timer;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = this.gameObject.GetComponent<GameManager>();
+        timer = FindObjectOfType<TimerController>();
+        worldSTIRate.fillAmount = (gameManager.WorldSTIRate + 10) / 100f;
+        worldTeenPregRate.fillAmount = (gameManager.WorldTeenPregRate + 10) / 100f;
+        worldCrimeRate.fillAmount = (gameManager.WorldCrimeRate + 10) / 100f;
+        financeBar.fillAmount = (gameManager.Finance + 10) / 100f;
+        educationBar.fillAmount = (gameManager.Education + 10) / 100f;
+    }
 
-		gameManager = this.gameObject.GetComponent<GameManager> ();
-		timer = FindObjectOfType <TimerController>();
-		worldSTIRate.fillAmount = gameManager.WorldSTIRate / 100f;
-		worldTeenPregRate.fillAmount = gameManager.WorldTeenPregRate / 100f;
-		worldCommHealthRate.fillAmount = gameManager.WorldCrimeRate / 100f;
-		financeBar.fillAmount = gameManager.Finance / 100f;
-		educationBar.fillAmount = gameManager.Education / 100f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (worldSTIRate.fillAmount < (gameManager.WorldSTIRate / 100f) - 0.01f)
+        {
+            worldSTIRate.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldSTIRate.fillAmount, Stats.STIRATE);
+        }
+        else if (worldSTIRate.fillAmount > (gameManager.WorldSTIRate / 100f) + 0.01f)
+        {
+            worldSTIRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldSTIRate.fillAmount, Stats.STIRATE);
+        }
 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        if (worldCrimeRate.fillAmount < (gameManager.WorldCrimeRate / 100f) - 0.01f)
+        {
+            worldCrimeRate.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldCrimeRate.fillAmount, Stats.CRIMERATE);
+        }
+        else if (worldCrimeRate.fillAmount > (gameManager.WorldCrimeRate / 100f) + 0.01f)
+        {
+            worldCrimeRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldCrimeRate.fillAmount, Stats.CRIMERATE);
+        }
 
-		if (worldSTIRate.fillAmount < (gameManager.WorldSTIRate/100f))
-		worldSTIRate.fillAmount += changeOverTime/ timer.TimeLimit* Time.deltaTime ;
-		if (worldSTIRate.fillAmount > (gameManager.WorldSTIRate/100f))
-			worldSTIRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
-		
-		
-		if (worldCommHealthRate.fillAmount < (gameManager.WorldCrimeRate/100f))
-			worldCommHealthRate.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
-		if (worldCommHealthRate.fillAmount > (gameManager.WorldCrimeRate / 100f))
-			worldCommHealthRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+        if (worldTeenPregRate.fillAmount < (gameManager.WorldTeenPregRate / 100f) - 0.01f)
+        {
+            worldTeenPregRate.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldTeenPregRate.fillAmount, Stats.TEENPREGRATE);
+        }
+        else if (worldTeenPregRate.fillAmount > (gameManager.WorldTeenPregRate / 100f) + 0.01f)
+        {
+            worldTeenPregRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+            UpdateColor(worldTeenPregRate.fillAmount, Stats.TEENPREGRATE);
+        }
 
-		
-		if (worldTeenPregRate.fillAmount < (gameManager.WorldTeenPregRate / 100f))
-			worldTeenPregRate.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
-		if (worldTeenPregRate.fillAmount > (gameManager.WorldTeenPregRate / 100f))
-			worldTeenPregRate.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+        if (financeBar.fillAmount < (gameManager.Finance / 100f) - 0.01f)
+        {
+            financeBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
+        }
+        if (financeBar.fillAmount > (gameManager.Finance / 100f) + 0.01f)
+        {
+            financeBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+        }
 
-		if (financeBar.fillAmount < (gameManager.Finance / 100f))
-			financeBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
-		if (financeBar.fillAmount > (gameManager.Finance / 100f))
-			financeBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+        if (educationBar.fillAmount < (gameManager.Education / 100f) - 0.01f)
+        {
+            educationBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
+        }
+        if (educationBar.fillAmount > (gameManager.Education / 100f) + 0.01f)
+        {
+            educationBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
+        }
+    }
 
-		if (educationBar.fillAmount < (gameManager.Education / 100f))
-			educationBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
-		if (educationBar.fillAmount > (gameManager.Education / 100f))
-			educationBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
-	
-	}
+    private void UpdateColor(float healthAverage, Stats relevantStat)
+    {
+        Color currentColor = new Color();
+
+        if (healthAverage < 0.5f)
+        {
+            currentColor = new Color(healthAverage * 2, 1, 0);
+        }
+        else if (healthAverage == 0.5f)
+        {
+            currentColor = new Color(1, 1, 0);
+        }
+        else if (healthAverage > 0.5f)
+        {
+            currentColor = new Color(1, (1 - healthAverage) * 2, 0);
+        }
+
+        switch (relevantStat)
+        {
+            case Stats.CRIMERATE:
+                worldCrimeRate.color = currentColor;
+                break;
+            case Stats.STIRATE:
+                worldSTIRate.color = currentColor;
+                break;
+            case Stats.TEENPREGRATE:
+                worldTeenPregRate.color = currentColor;
+                break;
+            default:
+                Debug.LogWarning("This Stat does not exist");
+                break;
+        }
+    }
 }
--- Assets/Scripts/StatsBar.cs
+++ Assets/Scripts/StatsBar.cs
@@ -6,57 +6,54 @@ public class StatsBar : MonoBehaviour {
 
 	public Image stiRateBar, teenPregRateBar, crimeRateBar;
     public Color CurrentColor = new Color();
-	private float changeOverTime = 5.0f;
+	private float changeOverTime = 15.0f;
 	private TargetLocation location;
 	private TimerController timer;
 
-
-
-	// Use this for initialization
-	void Start () {
-	
-		location = this.GetComponent<TargetLocation> ();
-		timer = FindObjectOfType<TimerController> ();
-		stiRateBar.fillAmount = location.STIRate / 100f;
-		teenPregRateBar.fillAmount = location.TeenPregRate / 100f;
-		crimeRateBar.fillAmount = location.CrimeRate/ 100f;
-	}
-
-    // Update is called once per frame
+    void Start()
+    {
+        location = this.GetComponent<TargetLocation>();
+        timer = FindObjectOfType<TimerController>();
+        stiRateBar.fillAmount = (location.STIRate + 10) / 100f;
+        teenPregRateBar.fillAmount = (location.TeenPregRate + 10) / 100f;
+        crimeRateBar.fillAmount = (location.CrimeRate + 10) / 100f;
+    }
+    
     void Update()
     {
-        if (stiRateBar.fillAmount < (location.STIRate / 100f))
+        if (stiRateBar.fillAmount < (location.STIRate / 100f) - 0.01f)
         {
             stiRateBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(stiRateBar.fillAmount, Stats.STIRATE);
         }
-        else if (stiRateBar.fillAmount > (location.STIRate / 100f))
+        else if (stiRateBar.fillAmount > (location.STIRate / 100f) + 0.01f)
         {
             stiRateBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(stiRateBar.fillAmount, Stats.STIRATE);
         }
-        
-        if (teenPregRateBar.fillAmount < (location.TeenPregRate / 100f))
+
+        if (teenPregRateBar.fillAmount < (location.TeenPregRate / 100f) - 0.01f)
         {
             teenPregRateBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(teenPregRateBar.fillAmount, Stats.TEENPREGRATE);
         }
-        else if (teenPregRateBar.fillAmount > (location.TeenPregRate / 100f))
+        else if (teenPregRateBar.fillAmount > (location.TeenPregRate / 100f) + 0.01f)
         {
             teenPregRateBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(teenPregRateBar.fillAmount, Stats.TEENPREGRATE);
         }
 
-        if (crimeRateBar.fillAmount < (location.CrimeRate / 100f))
+        if (crimeRateBar.fillAmount < (location.CrimeRate / 100f) - 0.01f)
         {
             crimeRateBar.fillAmount += changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(crimeRateBar.fillAmount, Stats.CRIMERATE);
         }
-        else if (crimeRateBar.fillAmount > (location.CrimeRate / 100f))
+        else if (crimeRateBar.fillAmount > (location.CrimeRate / 100f) + 0.01f)
         {
             crimeRateBar.fillAmount -= changeOverTime / timer.TimeLimit * Time.deltaTime;
             UpdateColor(crimeRateBar.fillAmount, Stats.CRIMERATE);
         }
+        //Debug.Log(crimeRateBar.fillAmount);
     }
 
     private void UpdateColor(float healthAverage, Stats relevantStat)
@@ -65,7 +62,7 @@ public class StatsBar : MonoBehaviour {
 
         if (healthAverage < 0.5f)
         {
-            currentColor = new Color(1, healthAverage * 2, 0);
+            currentColor = new Color(healthAverage * 2, 1, 0);
         }
         else if (healthAverage == 0.5f)
         {
@@ -73,7 +70,7 @@ public class StatsBar : MonoBehaviour {
         }
         else if (healthAverage > 0.5f)
         {
-            currentColor = new Color(1 - ((1 - healthAverage) * 2), 1, 0);
+            currentColor = new Color(1, (1 - healthAverage) * 2, 0);
         }
 
         switch (relevantStat)
