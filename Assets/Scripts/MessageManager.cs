38a5043e69a8076c84e34a5155816dd193b4e839
mwarrington <mason.arrington@gmail.com>
2018-01-05 16:37:15 -0600

    basic messaging system works

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class MessageManager : MonoBehaviour
 {
     public GameObject MessageBox;
+    public EventInfo Temp;
 
     private Text _title,
                  _messageText;
@@ -13,10 +14,22 @@ public class MessageManager : MonoBehaviour
     private void Start()
     {
         _title = MessageBox.transform.Find("Title").GetComponent<Text>();
+        _messageText = MessageBox.transform.Find("Title/MessageText").GetComponent<Text>();
+        MessageBox.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            ShowMessage(Temp);
     }
 
     public void ShowMessage(EventInfo messageEvent)
     {
+        MessageBox.SetActive(true);
+        _title.text = messageEvent.EventTitle;
+        _messageText.text = messageEvent.EventText;
 
+        //Event effect stuff here
     }
 }
--- Assets/Scripts/Scriptable Objects/EventInfo.cs
+++ Assets/Scripts/Scriptable Objects/EventInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class EventInfo : ScriptableObject
 {
@@ -8,3 +11,19 @@ public class EventInfo : ScriptableObject
                   EventText;
     public EventEffect TheEventEffect;
 }
+
+#if UNITY_EDITOR
+public class MakeEventInfo
+{
+    [MenuItem("Assets/Create/EventInfo")]
+    public static void CreateEventInfo()
+    {
+        EventInfo theEI = ScriptableObject.CreateInstance<EventInfo>();
+
+        AssetDatabase.CreateAsset(theEI, "Assets/Resources/VictoryConditions/NewEventInfo.asset");
+        AssetDatabase.SaveAssets();
+
+        Selection.activeObject = theEI;
+    }
+}
+#endif
