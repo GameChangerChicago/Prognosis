c6b0841583396110a3cfe7c4d12e5bce6d8d4c86
mwarrington <mason.arrington@gmail.com>
2018-01-08 15:40:09 -0600

    small fix

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class MessageManager : MonoBehaviour
 {
     public GameObject MessageBox;
-    public EventInfo Temp;
 
     private GameManager _theGameManager;
     private TargetLocation _ashPark,
@@ -30,129 +29,131 @@ public class MessageManager : MonoBehaviour
         _eastBeaHeights = GameObject.Find("WorldMap/East Bea Heights").GetComponent<TargetLocation>();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-            ShowMessage(Temp);
-    }
-
     public void ShowMessage(EventInfo messageEvent)
     {
         MessageBox.SetActive(true);
         _title.text = messageEvent.EventTitle;
         _messageText.text = messageEvent.EventText;
-
-        //Event effect stuff here
-        EventEffect EE = messageEvent.TheEventEffect;
-        for (int i = 0; i < EE.LocationToAffect.Count; i++)
+        
+        if(messageEvent.TheEventEffect != null)
         {
-            switch (EE.LocationToAffect[i])
+            EventEffect EE = messageEvent.TheEventEffect;
+
+            for (int i = 0; i < EE.LocationToAffect.Count; i++)
             {
-                case Locations.GLOBAL:
-                    switch(EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _theGameManager.WorldCrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _theGameManager.WorldSTIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _theGameManager.WorldTeenPregRate += EE.StatChange[i];
-                            break;
-                        case Stats.FINANCE:
-                            _theGameManager.Finance += EE.StatChange[i];
-                            break;
-                        case Stats.EDUCATION:
-                            _theGameManager.Education += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case Locations.ASHPARK:
-                    switch (EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _ashPark.CrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _ashPark.STIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _ashPark.TeenPregRate += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case Locations.EASTBEAHEIGHTS:
-                    switch (EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _eastBeaHeights.CrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _eastBeaHeights.STIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _eastBeaHeights.TeenPregRate += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case Locations.FREEMASON:
-                    switch (EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _freemason.CrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _freemason.STIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _freemason.TeenPregRate += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case Locations.PHILMONT:
-                    switch (EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _philmont.CrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _philmont.STIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _philmont.TeenPregRate += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case Locations.QUINNSQUARE:
-                    switch (EE.StatToChange[i])
-                    {
-                        case Stats.CRIMERATE:
-                            _quinnSquare.CrimeRate += EE.StatChange[i];
-                            break;
-                        case Stats.STIRATE:
-                            _quinnSquare.STIRate += EE.StatChange[i];
-                            break;
-                        case Stats.TEENPREGRATE:
-                            _quinnSquare.TeenPregRate += EE.StatChange[i];
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
+                switch (EE.LocationToAffect[i])
+                {
+                    case Locations.GLOBAL:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _theGameManager.WorldCrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _theGameManager.WorldSTIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _theGameManager.WorldTeenPregRate += EE.StatChange[i];
+                                break;
+                            case Stats.FINANCE:
+                                _theGameManager.Finance += EE.StatChange[i];
+                                break;
+                            case Stats.EDUCATION:
+                                _theGameManager.Education += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case Locations.ASHPARK:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _ashPark.CrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _ashPark.STIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _ashPark.TeenPregRate += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case Locations.EASTBEAHEIGHTS:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _eastBeaHeights.CrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _eastBeaHeights.STIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _eastBeaHeights.TeenPregRate += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case Locations.FREEMASON:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _freemason.CrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _freemason.STIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _freemason.TeenPregRate += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case Locations.PHILMONT:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _philmont.CrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _philmont.STIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _philmont.TeenPregRate += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case Locations.QUINNSQUARE:
+                        switch (EE.StatToChange[i])
+                        {
+                            case Stats.CRIMERATE:
+                                _quinnSquare.CrimeRate += EE.StatChange[i];
+                                break;
+                            case Stats.STIRATE:
+                                _quinnSquare.STIRate += EE.StatChange[i];
+                                break;
+                            case Stats.TEENPREGRATE:
+                                _quinnSquare.TeenPregRate += EE.StatChange[i];
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
+    }
+
+    public void HideMessage()
+    {
+        MessageBox.SetActive(false);
     }
 }
