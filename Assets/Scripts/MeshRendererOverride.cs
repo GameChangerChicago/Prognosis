fa7aaf410d979f9d7e51759fbf0892df6219203e
mwarrington <mason.arrington@gmail.com>
2018-02-27 15:40:10 -0600

    Prof portraits in!

---
+++ Assets/Scripts/MeshRendererOverride.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRendererOverride : MonoBehaviour
+{
+    private void Start()
+    {
+        foreach (Renderer r in GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            r.sortingLayerName = "Default";
+            r.sortingOrder = 13;
+        }
+    }
+}
