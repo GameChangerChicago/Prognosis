a843d4d6fdd5016daa642203e345a087320fe0f7
mwarrington <mason.arrington@gmail.com>
2018-03-28 16:21:54 -0500

    Start of tool tips

---
+++ Assets/Scripts/TutorialToolTip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialToolTip : MonoBehaviour
+{
+    public GameObject ParentObject;
+    public TutorialToolTip NextTTT;
+
+    //Close Method
+}
---
+++ Assets/Scripts/TutorialToolTipManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialToolTipManager : MonoBehaviour
+{
+    public TutorialToolTip CurrentToolTip;
+
+    //Instantiation Logic
+
+    //Close Logic
+
+    //Instantiate Method
+}
