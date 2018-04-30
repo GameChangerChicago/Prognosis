88e7f782ad40bfa58ee51e62ac9e2c031c8bc032
Kinsey Erickson <kinseyerickson2019@u.northwestern.edu>
2018-04-30 11:25:28 -0500

    Timer script updated to include warning animation

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -8,10 +8,13 @@ public class TimerController : MonoBehaviour
                          PausePlay;
     public GameObject PPButtonHighlight,
                       FFButtonHighlight;
+
     public float Timer,
                  TimeLimit;
     public bool TimerActive;
-    private bool warningGiven = false;
+    private bool _warningGiven = false;
+    private bool _isRed = false;
+    
 	public Image clockRenderer;
 	public Text  timerText;
 
@@ -25,14 +28,17 @@ public class TimerController : MonoBehaviour
     {
         _gameManager = FindObjectOfType<GameManager>();
         _audioManager = AudioManager.instance;
-		//playRenderer.enabled = false;
-       // _pauseRenderer = PausePlay.GetComponentInChildren<SpriteRenderer>();
-		//playRenderer = PausePlay.GetComponentInChildren<SpriteRenderer> ();
+        
+        Timer = 70;
+        //playRenderer.enabled = false;
+        // _pauseRenderer = PausePlay.GetComponentInChildren<SpriteRenderer>();
+        //playRenderer = PausePlay.GetComponentInChildren<SpriteRenderer> ();
 
     }
 
     void Update()
     {
+
         if (TimerActive)
         {
             Timer += Time.deltaTime;
@@ -43,18 +49,15 @@ public class TimerController : MonoBehaviour
 			timerText.text = (_gameManager.CurrentTurn + 1).ToString();
         }
 
-        //if there are less than ten seconds left & a warning hasn't been given
-        if (Timer > (TimeLimit - 10) && !warningGiven)
-        {
-            _audioManager.PlaySound("Time Warning");
-            warningGiven = true;
-        }
+        updateTimerWarning(); //checks to see if warning needs to be given
 
         if (Timer > TimeLimit)
         {
             _gameManager.CurrentTurn++;
             Timer = 0;
-            warningGiven = false;
+            clockRenderer.color = new Color(1, 1, 1); //set to white
+            _warningGiven = false;
+
         }
     }
 
@@ -74,11 +77,35 @@ public class TimerController : MonoBehaviour
         TimerActive = !TimerActive;
     }
 
+    public void updateTimerWarning()
+    {
+        //if there are less than ten seconds left & a warning hasn't been given
+        if (Timer > (TimeLimit - 15) && !_warningGiven)
+        {
+            _audioManager.PlaySound("Time Warning");
+            _warningGiven = true;
+            clockRenderer.color = new Color(clockRenderer.color.r, 0, 0, clockRenderer.color.a); //sets to red
+            _isRed = true;
+        }
+
+        //finishes the warning animation
+        if (Timer > (TimeLimit - 14.7) && Timer < (TimeLimit - 14.4) && _isRed)
+        {
+            clockRenderer.color = new Color(1, 1, 1); //sets to white
+            _isRed = false;
+        }
+        if (Timer > (TimeLimit - 14.4) && Timer < (TimeLimit - 14.1) && !_isRed)
+        {
+            clockRenderer.color = new Color(1, 0, 0); //sets to red
+            _isRed = true;
+        }
+    }
+
     public void SkipToNextDay()
     {
         _gameManager.CurrentTurn++;
         Timer = 0;
-        warningGiven = false;
+        _warningGiven = false;
     }
     
     public void HighlightPausePlayOn()
